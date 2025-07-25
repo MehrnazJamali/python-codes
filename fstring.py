e007936e5c56f0931fa5d34059dc6d8021413d75
MehrnazJamali <mehrnazjamali79@gmail.com>
2025-07-25 23:34:31 +0330

    HW1-1

---
+++ fstring.py
@@ -0,0 +1,6 @@
+#fstring
+name = input("enter your name:")
+print (f"welcome {name}")
+
+age = eval(input("how old are you?"))
+print (f"after 10 years your age will be: {age + 10}")
---
+++ string.method.py
@@ -0,0 +1,18 @@
+name = "mehrnaz"
+last_name = "JAMALI MEHRABADI"
+text = "python is my favorite programming language."
+print (len(name))
+print (name.upper())
+print (last_name.lower())
+print (name.capitalize())
+print (text.title())
+print (text.count("o"))
+print (text.count("python"))
+print (text.replace("python","javascript"))
+print (name.isalpha())
+print (text.isalnum())
+n = "django"
+print (n.find("a"))
+print (n.index("n"))
+print (text[22:33])
+print (last_name[::-1])
