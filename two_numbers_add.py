cb7cc59a4d30d7c29b14e1e05eab020fcc513204
MehrnazJamali <mehrnazjamali79@gmail.com>
2025-07-25 21:38:09 +0330

    HW1

---
+++ two_numbers_add.py
@@ -0,0 +1,7 @@
+a = eval (input("enter num1:"))
+b = eval(input("enter num2:"))
+sum = a + b 
+print (sum)
+# a = eval (input("enter num1:"))
+# b = eval(input("enter num2:"))
+# print (a+b)
---
+++ two_numbers_mul.py
@@ -0,0 +1,3 @@
+a = eval(input("enter num1:"))
+b = eval(input("enter num2:"))
+print (a*b)
